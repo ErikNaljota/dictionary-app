8f0e435062af3b36fd0afefaf2b70c5e9419577f
ErikNaljota <enaljota@gmail.com>
2020-01-13 22:24:28 +0100

    added word class

---
+++ src/app/dictionary/word.ts
@@ -0,0 +1,9 @@
+export class Word {
+  word: string[][] = [['english', 'hello'], ['dutch', 'hoi'], ['estonian', 'tere']];
+
+  generateMap(): Map {
+    const map = new Map();
+    this.word.forEach(word1 => map.set(word1[0], word1[1]));
+    return map;
+  }
+}
